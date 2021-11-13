b1518744491c8e92eaf1ef28c0c18cc9ebb461ef
rokydas <droky136@gmail.com>
2021-11-13 17:05:41 +0600

    first commit

---
+++ src/components/About_us/developers.js
@@ -0,0 +1,20 @@
+export const Developers = [
+	{
+		id: 1,
+		name: "Prateek Joshi",
+		email: "prateekjoshi946@gmail.com",
+		img: "https://i.ibb.co/PMVHsCF/project2.jpg",
+		branch: "Your branch",
+		github: "https://github.com/Prateek1912",
+		linkedin: "https://www.linkedin.com/in/prateek-joshi-076389170/",
+	},
+	{
+		id: 2,
+		name: "Siddharth Rai",
+		email: "Siddharth.09rai1999@gmail.com",
+		img: "https://i.ibb.co/chMMNgt/project1.jpg",
+		branch: "Your branch",
+		github: "https://github.com/siddharth-rai",
+		linkedin: "https://www.linkedin.com/in/siddharth-rai-387363173/",
+	},
+];
